dba8386dc444fb32290dfed41214adcb857726d5
vjoos <vanessa.joos@smail.th-koeln.de>
2023-10-30 11:25:04 +0100

    Hello World

--- src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/OguzJoosIssaGdwWs2223Application.kt
+++ src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/OguzJoosIssaGdwWs2223Application.kt
@@ -2,9 +2,21 @@ package de.thkoeln.gm.oguz_joos_issa_gdw_ws2223
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 @SpringBootApplication
 class OguzJoosIssaGdwWs2223Application
+
+@Controller
+class GreetingsController {
+    @GetMapping("/")
+    @ResponseBody
+    fun sayHelloWorld(): String {
+        return "Hello World"
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<OguzJoosIssaGdwWs2223Application>(*args)
