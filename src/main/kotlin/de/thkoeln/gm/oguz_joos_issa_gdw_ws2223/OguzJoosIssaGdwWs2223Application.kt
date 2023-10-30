25ae34e6cb0cdaacae679cdb954ec02f7cae1209
Leyla Issa <leyla.issa@smail.th-koeln.de>
2023-10-30 10:33:35 +0100

    Initial commit

---
+++ src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/OguzJoosIssaGdwWs2223Application.kt
@@ -0,0 +1,11 @@
+package de.thkoeln.gm.oguz_joos_issa_gdw_ws2223
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OguzJoosIssaGdwWs2223Application
+
+fun main(args: Array<String>) {
+    runApplication<OguzJoosIssaGdwWs2223Application>(*args)
+}
---
+++ src/test/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/OguzJoosIssaGdwWs2223ApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.thkoeln.gm.oguz_joos_issa_gdw_ws2223
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OguzJoosIssaGdwWs2223ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
