b0e551c3ff74aa612881f9bffd58349d5bf99398
Leyla Issa <leyla.issa@smail.th-koeln.de>
2023-10-30 12:13:53 +0100

    added body to MoviesController.kt

--- src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/movierecommendations/movies/MoviesController.kt
+++ src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/movierecommendations/movies/MoviesController.kt
@@ -1,4 +1,19 @@
 package de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.movies
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import java.util.*
+
+@Controller
 class MoviesController {
+
+    @GetMapping("/movies")
+    @ResponseBody
+    fun saveMovie(name: String): String {
+        var movie: Movie = Movie()
+        movie.name = name
+        movie.dateOfRelease = Date()
+        return movie.toString()
+    }
 }
