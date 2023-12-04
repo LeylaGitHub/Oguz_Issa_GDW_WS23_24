145adec6d6fcc2c53dab27f80e75256c38f8be3c
Iogz <93346244+Iogz@users.noreply.github.com>
2023-12-04 10:48:52 +0100

    Irmak

--- src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/OguzJoosIssaGdwWs2223Application.kt
+++ src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/OguzJoosIssaGdwWs2223Application.kt
@@ -5,6 +5,10 @@ import org.springframework.boot.runApplication
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 
 @SpringBootApplication
 class OguzJoosIssaGdwWs2223Application
@@ -18,6 +22,18 @@ class GreetingsController {
     }
 }
 
+/**
+ * Ruft die Feiertags-API auf und gibt sie als Strings aus
+ * */
 fun main(args: Array<String>) {
     runApplication<OguzJoosIssaGdwWs2223Application>(*args)
+
+    val client = HttpClient.newBuilder().build();
+    val request  = HttpRequest.newBuilder()
+        .GET()
+        .uri(URI.create("https://feiertage-api.de/api/?nur_land=NW"))
+        .build();
+    val response = client.send(request,HttpResponse.BodyHandlers.ofString());
+    println(response.body())
+
 }
