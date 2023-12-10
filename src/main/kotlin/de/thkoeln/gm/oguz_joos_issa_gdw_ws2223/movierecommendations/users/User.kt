c71bcdede746947a6b976df9ff821cdab822d68d
Iogz <93346244+Iogz@users.noreply.github.com>
2023-12-10 21:09:18 +0100

    class User

--- src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/movierecommendations/users/User.kt
+++ src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/movierecommendations/users/User.kt
@@ -1,5 +1,7 @@
 package de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.users
 
+import de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.genre.Genre
+import de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.movies.Movie
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
@@ -12,4 +14,10 @@ class User {
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     var id: UUID = UUID.randomUUID()
+    var name: String = ""
+    var mail: String = ""
+    var age: Int = 0
+    var favouriteMovies: MutableList<Movie> = mutableListOf()
+    var favouriteGenres: MutableList<Genre> = mutableListOf()
+
 }
