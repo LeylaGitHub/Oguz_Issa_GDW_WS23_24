34c7758e4b9351348dda5ff2a4954daa5179d1ad
Iogz <93346244+Iogz@users.noreply.github.com>
2023-12-11 20:45:53 +0100

    API

--- src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/OguzJoosIssaGdwWs2223Application.kt
+++ src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/OguzJoosIssaGdwWs2223Application.kt
@@ -5,6 +5,11 @@ import org.springframework.boot.runApplication
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
 
 @SpringBootApplication
 class OguzJoosIssaGdwWs2223Application
@@ -20,4 +25,22 @@ class GreetingsController {
 
 fun main(args: Array<String>) {
     runApplication<OguzJoosIssaGdwWs2223Application>(*args)
+
+    /**
+     * Zugriff auf die MCU API
+     *
+     */
+    val apiURL = "https://www.whenisthenextmcufilm.com/"
+
+    val client = HttpClient.newBuilder().build();
+    val request  = HttpRequest.newBuilder()
+        .GET()
+        .uri(URI.create(apiURL))
+        .build();
+    val response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    println(response.body())
+
+
+
 }
+
