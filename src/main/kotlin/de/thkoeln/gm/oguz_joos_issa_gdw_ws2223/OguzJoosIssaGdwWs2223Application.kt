c550cd2444514384d66fc6ed5fb33a37098a6389
Iogz <93346244+Iogz@users.noreply.github.com>
2023-12-11 21:03:57 +0100

    mcuApiUrl & imbdApiUrl implemented, GET-method

--- src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/OguzJoosIssaGdwWs2223Application.kt
+++ src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/OguzJoosIssaGdwWs2223Application.kt
@@ -26,21 +26,35 @@ class GreetingsController {
 fun main(args: Array<String>) {
     runApplication<OguzJoosIssaGdwWs2223Application>(*args)
 
-    /**
-     * Zugriff auf die MCU API
-     *
-     */
-    val apiURL = "https://www.whenisthenextmcufilm.com/"
+    // Beispiel für https://www.whenisthenextmcufilm.com/
+    // -- in Bearbeitung --
+    val mcuApiURL = "https://www.whenisthenextmcufilm.com/"
 
-    val client = HttpClient.newBuilder().build();
-    val request  = HttpRequest.newBuilder()
+    val mcuClient = HttpClient.newHttpClient()
+    val mcuRequest = HttpRequest.newBuilder()
         .GET()
-        .uri(URI.create(apiURL))
-        .build();
-    val response = client.send(request, HttpResponse.BodyHandlers.ofString());
-    println(response.body())
+        .uri(URI.create(mcuApiURL))
+        .build()
 
+    val mcuResponse = mcuClient.send(mcuRequest, HttpResponse.BodyHandlers.ofString())
+    println("API-Antwort für MCU: ${mcuResponse.body()}")
 
+    // Beispiel für https://imdb-api.com/en/API/Title/k_12345678/tt1375666
+    // -- in Bearbeitung --
+
+    val apiKey = "dc7d9197471301b4662f3e024d3643d6"
+    val imdbId = "tt1375666" //  IMDb-ID des gesuchten Films
+
+    val imdbApiURL = "https://imdb-api.com/en/API/Title/k_$apiKey/$imdbId"
+
+    val imdbClient = HttpClient.newHttpClient()
+    val imdbRequest = HttpRequest.newBuilder()
+        .GET()
+        .uri(URI.create(imdbApiURL))
+        .build()
+
+    val imdbResponse = imdbClient.send(imdbRequest, HttpResponse.BodyHandlers.ofString())
+    println("API-Antwort für IMDb: ${imdbResponse.body()}")
 
 }
 
