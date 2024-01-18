183d79de7bd871a9a6bc13b2a643e791f84cc0fb
Leyla Issa <leyla.issa@smail.th-koeln.de>
2024-01-18 18:08:00 +0100

    deleted folder Genre and implemented FavoriteMovie.kt

--- src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/movierecommendations/favoriteMovies/FavoriteMovie.kt
+++ src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/movierecommendations/favoriteMovies/FavoriteMovie.kt
@@ -1,4 +1,24 @@
 package de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.favoriteMovies
 
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import org.hibernate.annotations.GenericGenerator
+import de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.movies.Movie
+import de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.users.User
+import jakarta.persistence.OneToOne
+import java.util.*
+
+@Entity
 class FavoriteMovie {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name="uuid2", strategy = "uuid2")
+    var id: UUID = UUID.randomUUID()
+    var name: String = ""
+    var number: Int = 0
+    var content: MutableList<Movie> = mutableListOf()
+
+    @OneToOne
+    var user: User? = null
 }
--- src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/movierecommendations/genre/GenresService.kt
+++
@@ -1,4 +0,0 @@
-package de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.genre
-
-class GenresService {
-}
