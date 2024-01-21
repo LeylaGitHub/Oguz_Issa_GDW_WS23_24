ca7ee20e086e81b9c729df56a34c96c9cf8b066f
Leyla Issa <leyla.issa@smail.th-koeln.de>
2024-01-21 21:33:23 +0100

    updated MoviesService.kt and MoviesServiceImpl.kt according to REST-Table

--- src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/movierecommendations/movies/MoviesServiceImpl.kt
+++ src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/movierecommendations/movies/MoviesServiceImpl.kt
@@ -1,32 +1,34 @@
 package de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.movies
 
-import de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.genre.Genre
-import de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.users.User
 import org.springframework.data.repository.findByIdOrNull
 import java.util.*
 
-class MoviesServiceImpl (private val moviesRepository: MoviesRepository) : MoviesService {
+abstract class MoviesServiceImpl (private val moviesRepository: MoviesRepository) : MoviesService {
     override fun findById(id: UUID): Movie? {
         return moviesRepository.findByIdOrNull(id)
     }
 
-    override fun findAllMovies(): List<Movie> {
+    override fun getAllMovies(): List<Movie> {
         return moviesRepository.findAll().toList()
     }
 
-    override fun findByGenre(genre: Genre): List<Movie> {
-        return findAllMovies().filter { TODO() }.toList()
+    override fun findByGenre(genre: String): List<Movie> {
+        return getAllMovies().filter { TODO()/* X == genre */ }.toList()
     }
 
-    override fun getAllFavByUser(user: User): List<Movie> {
-        return moviesRepository.findByFavUser(user).toList()
+    override fun generateMovie(movie: Movie) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateMovie(movie: Movie) {
+        TODO("Not yet implemented")
+    }
+
+    override fun save(movie: Movie) {
+        moviesRepository.save(movie)
     }
 
     override fun delete(movie: Movie) {
         moviesRepository.delete(movie)
-    }
-
-    override fun favourise(movie: Movie) {
-        moviesRepository.save(movie)
     }
 }
