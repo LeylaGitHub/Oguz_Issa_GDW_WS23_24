02ffe7ab4f7917aa6b255b84eaae7be03de5bea6
Iogz <93346244+Iogz@users.noreply.github.com>
2024-01-21 03:09:04 +0100

    FavoriteMovieController added, functions implemented

--- src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/movierecommendations/favoriteMovies/FavoriteMovieRespository.kt
+++ src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/movierecommendations/favoriteMovies/FavoriteMovieRespository.kt
@@ -1,4 +1,11 @@
 package de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.favoriteMovies
 
-interface FavoriteMovieRespository {
+import de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.users.User
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface FavoriteMovieRespository : CrudRepository<User, UUID> {
+
 }
--- src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/movierecommendations/favoriteMovies/FavoriteMoviesService.kt
+++ src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/movierecommendations/favoriteMovies/FavoriteMoviesService.kt
@@ -1,11 +1,14 @@
 package de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.favoriteMovies
 
 import de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.movies.Movie
+import de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.users.User
+import java.util.*
 
 interface FavoriteMoviesService {
-    fun findByMovieId(favoriteMovie: FavoriteMovie)
-    fun getAllFavByUser(favoriteMovies: FavoriteMovie)
-    fun favourise(movie: Movie)
-    fun updateFavMov(favoriteMovie: FavoriteMovie)
-    fun delete(favoriteMovie: FavoriteMovie)
+    fun findAll(): List<FavoriteMovie>
+    fun save (favoriteMovie: FavoriteMovie): FavoriteMovie
+    fun findById(favMovieId: UUID): FavoriteMovie?
+    fun existsById(favMovieId: UUID): Boolean
+    fun deleteById (favMovieId: UUID)
+
 }
