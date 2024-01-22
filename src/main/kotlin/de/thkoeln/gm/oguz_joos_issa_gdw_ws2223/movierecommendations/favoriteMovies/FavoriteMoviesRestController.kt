fe926324aa9b4730b516d6d15d8db73a5b1b917a
Leyla Issa <leyla.issa@smail.th-koeln.de>
2024-01-22 11:30:46 +0100

    deleted FavoriteMoviesController.kt

--- src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/movierecommendations/favoriteMovies/FavoriteMoviesRestController.kt
+++ src/main/kotlin/de/thkoeln/gm/oguz_joos_issa_gdw_ws2223/movierecommendations/favoriteMovies/FavoriteMoviesRestController.kt
@@ -1,28 +1,68 @@
 package de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.favoriteMovies
 
-import de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.movies.Movie
-import de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.users.User
-import de.thkoeln.gm.oguz_joos_issa_gdw_ws2223.movierecommendations.users.UsersService
-import org.springframework.data.crossstore.ChangeSetPersister
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.data.jpa.domain.AbstractPersistable_.id
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 import java.util.*
+import kotlin.NoSuchElementException
+
+data class FavMovieDTO(
+    val userId: UUID,
+    val name: String,
+)
 
 @RestController
-class FavoriteMoviesRestController (private val favoriteMoviesService: FavoriteMoviesService, private val usersService: UsersService) {
+@RequestMapping("api/v4")
+class FavoriteMoviesRestController (private val favoriteMoviesService: FavoriteMoviesService) {
 
-    @GetMapping("/favoriteMovies")
-    fun getFavMovies(@PathVariable userId: UUID) {
-        val user: User? = usersService.findById(userId)
-        if(user != null) {
-            return favoriteMoviesService.
-        } else {
-            throw ChangeSetPersister.NotFoundException()
+
+    @GetMapping ("/favoriteMovies")
+    fun getAll(): List<FavoriteMovie> {
+        val favMovies = favoriteMoviesService.findAll()
+
+        if (favMovies.isEmpty()) {
+            throw NoSuchElementException ("No favorite Movie found.")
         }
+
+        return favMovies
     }
 
-    @PostMapping("/users/{userId}/movies")
-    fun
+    @PostMapping("/favoriteMovies")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createFavMovie(@RequestBody newFavMovie: FavoriteMovie) : FavoriteMovie {
+        return favoriteMoviesService.save(newFavMovie)
+
+    }
+
+
+    @PatchMapping("/favoriteMovies/{movieId}")
+    fun updateFavMovie(@PathVariable favMovieId: UUID, @RequestBody updateFavoriteMovie: FavoriteMovie) : FavoriteMovie {
+        val existingFavMovie = favoriteMoviesService.findById(favMovieId) ?: throw NoSuchElementException()
+
+        existingFavMovie.apply {
+            name = updateFavoriteMovie.name
+        }
+
+        return favoriteMoviesService.save(existingFavMovie)
+    }
+
+
+    @GetMapping ("/favoriteMovies/{movieId}")
+    fun getFavMovie (@PathVariable favMovieId: UUID) : FavMovieDTO {
+        val favMovie = favoriteMoviesService.findById(favMovieId) ?: throw NoSuchElementException()
+
+        return FavMovieDTO(favMovie.id, favMovie.name)
+    }
+
+
+    @DeleteMapping("/favoriteMovies/{movieId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteFavMovie (@PathVariable favMovieId: UUID) {
+        if (!favoriteMoviesService.existsById(favMovieId)) {
+            throw NoSuchElementException("Favorite Movie not found with ID: $id")
+        }
+        favoriteMoviesService.deleteById(favMovieId)
+    }
+
+
 }
